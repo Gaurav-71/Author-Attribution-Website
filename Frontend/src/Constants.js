822d3db96c4b666d09fcacf4a6c64a98479fbf67
Gaurav V <gv71.msrit@gmail.com>
2023-06-15 13:43:42 +0530

    Updated prodUrl

--- Frontend/src/Constants.js
+++ Frontend/src/Constants.js
@@ -1,4 +1,4 @@
 export const devUrl = "http://127.0.0.1:9090/";
-export const prodUrl = "https://server.kagapa.com/";
+export const prodUrl = "https://kagapa.com/authorship39/";
 export const prodUrlOld = "http://20.235.72.222/";
 export const baseUrl = prodUrl;
